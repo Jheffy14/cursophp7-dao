8376e3602fa620cc8b1a31cc024a36767fc9f011
jeffy14 <jefferson_faria@outlook.com>
2024-05-13 13:47:41 -0300

    metodos de lista

--- class/Sql.php
+++ class/Sql.php
@@ -1,48 +1,50 @@
 <?php
 
-class Sql {
+class Sql extends PDO {
 
     private $conn;
 
     public function __construct() {
-        try {
-            $this->conn = new PDO("mysql:host=localhost;dbname=dbphp7", "root", "");
-            $this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-        } catch(PDOException $e) {
-            // Handle connection error
-            echo "Connection failed: " . $e->getMessage();
-        }
+
+        $this->conn = new PDO("mysql:host=localhost;dbname=dbphp7", "root", "root");
+
     }
 
     private function setParams($statement, $parameters = array()) {
+
         foreach ($parameters as $key => $value) {
+
             $this->setParam($statement, $key, $value);
+
         }
+
     }
 
-    private function setParam($statement, $key, $value) {
+    private function setParam($statement, $key, $value){
+
         $statement->bindParam($key, $value);
+
     }
 
     public function query($rawQuery, $params = array()) {
-        try {
-            $stmt = $this->conn->prepare($rawQuery);
-            $this->setParams($stmt, $params);
-            $stmt->execute();
-            return $stmt;
-        } catch(PDOException $e) {
-            // Handle query error
-            echo "Query failed: " . $e->getMessage();
-            return false;
-        }
+
+        $stmt = $this->conn->prepare($rawQuery);
+
+        $this->setParams($stmt, $params);
+
+        $stmt->execute();
+
+        return $stmt;
+
     }
 
-    public function select($rawQuery, $params = array()): array {
+    public function select($rawQuery, $params = array()):array
+    {
+
         $stmt = $this->query($rawQuery, $params);
-        if ($stmt) {
-            return $stmt->fetchAll(PDO::FETCH_ASSOC);
-        }
-        return array(); // or handle error as needed
+
+        return $stmt->fetchAll(PDO::FETCH_ASSOC);
+
     }
 
 }
--- index.php
+++ index.php
@@ -1,15 +1,30 @@
 <?php
 
 require_once("config.php");
-/* 						Carrega somente um usuário.
-$root = new Usuario();
 
-$root->loadbyId(3);
+//Carrega um usuário
+//$root = new Usuario();
+//$root->loadbyId(3);
+//echo $root;
 
-echo $root;
-*/
+//Carrega uma lista de usuários
+//$lista = Usuario::getList();
+//echo json_encode($lista);
 
-$lista 
+//Carrega uma lista de usuários buscando pelo login
+//$search = Usuario::search("jo");
+//echo json_encode($search);
 
+//Carrega um usuário usando o login e a senha
+//$usuario = new Usuario();
+//$usuario->login("root", "!@#$");
+
+//echo $usuario;
+
+$aluno = new Usuario("aluno", "@lun0");
+
+$aluno->insert();
+
+echo $aluno;
 
 ?>
